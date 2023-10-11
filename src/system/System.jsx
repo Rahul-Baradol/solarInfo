13d1df13d7f4297805c450954a2ecdb1eef21783
rahul <rahul.baradol.14@gmail.com>
2023-10-11 15:41:16 +0530

    Added the scaleOut functionality on scroll

--- src/system/System.jsx
+++ src/system/System.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './system.css'
+import { useRef } from 'react'
 
 const System = () => {
-  return (
+  const main = useRef(null); // main is like a pointer | main.current = *main
+  const [scaleFactor, setScaleFactor] = useState(620);
+
+  let style = {
+    transform: `scale(${scaleFactor}%)`,
+  }
+
+  useEffect(()=>{
+      if (scaleFactor < 100) {
+        main.current.addEventListener('wheel', (e)=>{});
+      }
+  }, [scaleFactor])
+
+  useEffect(()=>{
+    main.current.addEventListener('wheel', (e)=>{
+        if (e.deltaY > 0) {
+          setScaleFactor((current) => {
+            if (current < 100) return current;
+            return current - 1;
+          })
+        }
+    })
+  }, [main])
+
+  return ( 
     <>
-        <div className="main">
+        <div ref={main} className={`main`} style={style}>
             <div className="sun"></div>
             <div className="mercury"></div>
             <div className="venus"></div>    
