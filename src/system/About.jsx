530712842973bef01dcdbfc13df019edd0db6bc3
rahul <rahul.baradol.14@gmail.com>
2023-11-02 11:36:57 +0530

    fixed top margin

--- src/system/About.jsx
+++ src/system/About.jsx
@@ -3,8 +3,8 @@ import Profile from './profilessss/Profile'
 
 function About() {
   return (
-    <div className='overflow-scroll overflow-x-hidden scr w-[100vw] h-[100vh] flex flex-col items-start ml-3 sm:ml-7 mt-5 gap-6'>
-      <div className={`text-white text-3xl`} >ABOUT</div>
+    <div className='overflow-scroll overflow-x-hidden w-[100vw] h-[100vh] flex flex-col items-start ml-3 sm:ml-7 gap-6'>
+      <div className={`text-white text-3xl mt-5`} >ABOUT</div>
       <div className='text-white w-[90vw]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptatibus quia animi eligendi natus, veritatis vitae dolor? Corporis ut nulla et, tenetur porro repellendus molestias animi tempore dolores error enim.</div>
       <div className='flex flex-col gap-6'>
           <Profile data-image="" data-content="" link1="" link2="" link3=""/>
--- src/system/profilessss/Profile.jsx
+++ src/system/profilessss/Profile.jsx
@@ -3,8 +3,8 @@ function Profile() {
   return (
     <>
         <div className='border-2 w-[96.5vw] h-[30vh]'>
-            {/* <div className='image'></div>
-            <div className='content'></div> */}
+            {/* <div className=''></div>
+            <div className=''></div> */}
         </div>
     </>
 
