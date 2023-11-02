2ab0613f5d6b398c180aac69973ea0f5c78ab088
rahul <rahul.baradol.14@gmail.com>
2023-11-02 11:24:23 +0530

    Added about page

---
+++ src/system/profilessss/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+function Profile() {
+  return (
+    <>
+        <div className='border-2 w-[96.5vw] h-[30vh]'>
+            {/* <div className='image'></div>
+            <div className='content'></div> */}
+        </div>
+    </>
+
+  )
+}
+
+export default Profile
