b27e4d1b01b5f36b7b8fb0c391476a2ac94b7c2c
rahul <rahul.baradol.14@gmail.com>
2023-11-06 14:55:41 +0530

    (rishith) improved about page

--- src/system/profilessss/Profile.jsx
+++ src/system/profilessss/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-function Profile() {
+function Profile(p) {
+  let ifrotate="180deg"
   return (
     <>
-        <div className='border-2 w-[96.5vw] h-[30vh]'>
+        <div className={`rounded-l-full w-[96.5vw] h-[30vh] profile -rotate-[${ (+(p.rotate)) && ifrotate}]`} style={p.styles}>
             {/* <div className=''></div>
             <div className=''></div> */}
         </div>
