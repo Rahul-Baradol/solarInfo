527dce16d5fdcfe6524a03a7b9a37fa7f5b0c3f8
rahul <rahul.baradol.14@gmail.com>
2023-11-13 19:27:08 +0530

    cors

--- server/src/index.js
+++ server/src/index.js
@@ -4,15 +4,12 @@ const app = express();
 const port = 8000;
 const planets = require('./routes/planets');
 
-app.use(cors({ origin: "*" }))
-
 app.use((req, res, next) => {
     const allowedOrigins = ['http://localhost:3000', 'https://solar-info-frontend.vercel.app/'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
@@ -29,4 +26,4 @@ app.listen(port, () => {
     console.log(`Server listening at port ${port}...`);
 });
 
-export default app
+module.exports = app
